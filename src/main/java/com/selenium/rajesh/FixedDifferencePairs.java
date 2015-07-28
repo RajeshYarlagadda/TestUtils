ab3f142ce5f513ccb14e33deaa283f90bd8d0f20
RajeshYarlagadda <rajesh_krc_hyd@yahoo.com>
2015-07-28 18:01:26 +0800

    Fixed difference pairs

---
+++ src/main/java/com/selenium/rajesh/FixedDifferencePairs.java
@@ -0,0 +1,34 @@
+package com.selenium.rajesh;
+
+import java.util.Scanner;
+
+public class FixedDifferencePairs {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number of integers and difference:");
+		Scanner scanner = new Scanner(System.in);
+		int noofIntegers = scanner.nextInt();
+		int difference = scanner.nextInt();
+
+		System.out.println("Enter the "+noofIntegers+"  of integers :");
+		Integer[] integers = new Integer[noofIntegers];
+		for(int i = 0;i<noofIntegers;i++){
+			integers[i] = scanner.nextInt();
+		}
+		int count = new FixedDifferencePairs().findPair(integers, integers.length, difference);
+		System.out.println("number of pairs "+count);
+	}
+	
+	int findPair(Integer arr[], int size, int n){
+		int counter = 0;
+	    for(int i =0;i<size;i++){
+	    	for(int j=1;j<size;j++){
+	    		if(arr[j]-arr[i]==n){
+	    			System.out.println("Pair Found:  ("+ arr[i]+","+arr[j]+")");
+	    			counter++;
+	    		}
+	    	}
+	    }
+	    return counter;
+	}
+}
