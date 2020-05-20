fff44d8ecf74ddec8a609483b0fcf21d84376a2a
RajeshYarlagadda <rajesh_krc_hyd@yahoo.com>
2020-05-20 18:10:31 +0800

    removed url
    removed url

--- src/main/java/com/selenium/rajesh/DBConnection.java
+++ src/main/java/com/selenium/rajesh/DBConnection.java
@@ -9,10 +9,10 @@ public class DBConnection {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
-		/*java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://10.26.60.107/hrms","root","passwordiot");
+		/*java.sql.Connection conn = DriverManager.getConnection("","root","passwordiot");
 		Statement st = conn.createStatement();
 		ResultSet rs = st.executeQuery("select * from widgetmanagement");*/
-		java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://10.28.14.70:3306/CMS","root","0tRsr00t");
+		java.sql.Connection conn = DriverManager.getConnection("","root","0tRsr00t");
 		Statement st = conn.createStatement();
 		ResultSet rs = st.executeQuery("select * from MASTER_ACCESS_RIGHTS where CODE like 'MENU_04_%'");
 		if(rs.next())
